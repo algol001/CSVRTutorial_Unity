31e12b9e6493dd845c2aafd4c3760b1fad08f98d
algol001 <alina.goldman@gmail.com>
2015-07-24 03:28:01 -0400

    Version 1

---
+++ Assets/ScaleObjectDistance.js
@@ -0,0 +1,38 @@
+﻿ #pragma strict
+  /*
+ //public var terrain : Terrain;
+ var detailObjectDistance : float;
+ var distance = 5000.0;
+
+function Start () {
+	Terrain.activeTerrain.detailObjectDistance = distance;
+}
+
+function Update () {
+ //Terrain.activeTerrain.detailObjectDistance = distance;
+ 
+    if (Terrain.activeTerrain.detailObjectDistance != distance)
+    {
+     Terrain.activeTerrain.detailObjectDistance = distance;
+    }
+}
+*/
+
+// #if UNITY_EDITOR
+// @ContextMenu( "Terrain Object Distance" )
+// function ScaleObjectDistance() 
+// {
+//
+//     // check if there is no terrain in the inspector, then use the current active terrain
+//     if ( !terrain )
+//     {
+//     	//window.alert("activate active terrain");
+//         terrain = Terrain.activeTerrain;
+//     }
+//     
+//     //set active terrain to 20000
+//
+//   
+//    
+// }
+// #endif
