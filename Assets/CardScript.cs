36374195eac222da46ae4f7661e6adae23591da9
algol001 <alina.goldman@gmail.com>
2015-08-20 18:31:50 -0400

    card interaction works

--- Assets/CardScript.cs
+++ Assets/CardScript.cs
@@ -5,9 +5,13 @@ public class CardScript : MonoBehaviour {
 
 	//public variables
 	public GameObject FPSPlayer;
+	
+	public GameObject Card;
+
 	public float speed;
 	public float finalScale; //final scale of the card
 	public float finalDistance; //distance from face
+	public float finalHeight; 
 
 	//private variables
 	bool pickedUp;  //whether or not picked up
@@ -24,6 +28,8 @@ public class CardScript : MonoBehaviour {
 			//set to picked up
 			pickedUp = true;
 
+			var particle = GetComponentInChildren<ParticleSystem>();
+			particle.emissionRate = 0;
 			//get the audio
 			var audio = GetComponent<AudioSource> ();
 			audio.Play();
@@ -43,9 +49,12 @@ public class CardScript : MonoBehaviour {
 		if (pickedUp) {
 			var step = speed * (Time.time - timeStart)/ 100.0f; //100 is scaling var
 			var target = FPSPlayer.GetComponent<Transform>(); //transform is position, getting current position of player
+			var cardTransform = Card.GetComponent<Transform>();
+
+			cardTransform.localRotation = Quaternion.Slerp(cardTransform.localRotation, Quaternion.identity, step);
 
 			transform.position = Vector3.Slerp(positionStart, //vector3 = 3d vector
-			                                   target.position + target.forward*finalDistance  //calculate vector in front of face 
+			                                   target.position + target.forward*finalDistance + target.up*finalHeight//calculate vector in front of face 
 			                                   // target.forward is player direction, distance is how far in direction card should be
 			                                   , step);
 			transform.rotation = Quaternion.Slerp (transform.rotation //quaternion is 4d vector, representation rotation
