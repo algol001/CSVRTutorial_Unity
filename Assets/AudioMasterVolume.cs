a3bffaf504de82eee684b8da69bc6e90ff0eceee
algol001 <alina.goldman@gmail.com>
2015-10-06 04:34:27 -0400

    ben fixed volume issues, created master volume

---
+++ Assets/AudioMasterVolume.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioMasterVolume : MonoBehaviour {
+	public float Volume;
+	AudioSource source;
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Volume = source.volume;
+		AudioListener.volume = Volume;
+	}
+}
